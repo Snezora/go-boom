d1dc9f50f578f19ac4491bfd654af92b918b3b53
Snezroamia <1221302721@student.mmu.edu.my>
2023-05-28 17:02:34 +0800

    Help Menu added

--- app/src/main/java/com/example/goboom/MainActivity.java
+++ app/src/main/java/com/example/goboom/MainActivity.java
@@ -3,15 +3,26 @@ package com.example.goboom;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.view.animation.Animation;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.main_menu);
     }
 
+    public void help(View view) {
+        setContentView(R.layout.help_menu);
+        Log.d("OUTPUT", "Help Menu is Accessed! \n  " +
+                "    1. s - Start a new game\n" +
+                "      2. x - Exit the game  \n" +
+                "      3. d - Draw cards from deck");
+    }
+
+    public void help_backbutton(View view) {
+        setContentView(R.layout.main_menu);
+    }
 }
